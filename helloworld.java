b457d8ea0d2230dd15431a3f509e245ff31b794a
sri165 <49423625+sri165@users.noreply.github.com>
2022-02-13 17:34:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World : Triggered by webhhok!");
+    System.out.println("Hello World : Triggered by webhhok2");
     }
 }
